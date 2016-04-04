09a5cfad06d3abf8ca36dcb93e7f95da3c3b2461
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-04-04 19:54:34 -0400

    ajoute deuxieme epreuve

--- routes/steganography.js
+++ routes/steganography.js
@@ -11,12 +11,31 @@ router.post('/',function(req,res,next){
   res.download(__dirname + '/../public/out.wav');
 });
 
+router.post('/steg2',function(req,res,next){
+  res.download(__dirname + '/../public/images/stegano2.jpg');
+});
+
+//JHI
+router.post('/answer2', function(req,res,next){
+  if (req.body.answer == "JHI")
+  {
+    //goodAnswer
+    console.log('Passed!');
+    res.render('index', { title: 'HackMe' });
+  }
+  else {
+    //Bad answer
+    console.log('BAD');
+    res.render('steganography2', { title: 'HackMe', error:true });
+  }
+});
+
 router.post('/answer', function(req,res,next){
   if (req.body.answer == "AEO")
   {
     //goodAnswer
     console.log('Passed!');
-    res.render('index', { title: 'HackMe' });
+    res.render('steganography2', { title: 'HackMe', error:false });
   }
   else {
     //Bad answer
