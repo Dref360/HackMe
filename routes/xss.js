294f678afd794f3072d1d66c9c10b19c3d1644ed
Bruno Cadorette <brunocadorette@outlook.com>
2016-04-12 21:54:04 -0400

    change backend to include every console.log

--- routes/xss.js
+++ routes/xss.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var jsdom = require("jsdom");
+
 var router = express.Router();
 
 /* GET home page. */
@@ -7,26 +8,41 @@ router.get('/', function(req, res, next) {
   res.render('xss', { title: 'HackMe', answer:"", won: false });
 });
 
+router.post('/', function(req, res, next){
+    xssChallenge({prefix: "<html><body><div id='answer'><ul><li>", answer: req.body.answer, suffix: "</li></ul></div></body></html>"}, res, "xss");
+});
+
+router.get('/2', function(req, res, next) {
+  res.render('xss2', { title: 'HackMe', answer:"", won: false });
+});
+
+router.post('/2', function(req, res, next){
+    function htmlEscape(s) {
+        return s.replace(/./g, function(x) {
+           return { '<': '&lt;', '>': '&gt;', '&': '&amp;', '"': '&quot;', "'": '&#39;' }[x] || x;       
+        });
+    }
+
+    var ans = "Bonjour <a href='#'>" + htmlEscape(req.body.answer) +"</a>";
+    xssChallenge({prefix: "<html><body><div id='answer'>", answer: ans, suffix: "</div></body></html>"}, res, "xss2");
+});
 
 function xssChallenge(htmlChallenge, res, page) {
     var html = htmlChallenge.prefix + htmlChallenge.answer + htmlChallenge.suffix;
     console.log("received html: " + html);
-    jsdom.env(html,
-    function(error, window) {
-        var won;
-        if (!error && window && window.document && window.document.getElementsByTagName("script").length > 0) {
-            won = true;
-        }
-        else{
-            won = false;
-        }
-        res.set("X-XSS-Protection", "0").render(page, { title: 'HackMe', answer: htmlChallenge.answer, won:won });
+    var won = false;
+    var window;
+    const virtualConsole = jsdom.createVirtualConsole();
+    virtualConsole.on("log", function (message) {
+        won = true;
     });
+    window = jsdom.jsdom(html, { virtualConsole }).defaultView;
+    setTimeout(function(){ 
+        window.close();
+        res.set("X-XSS-Protection", "0").render(page, { title: 'HackMe', answer: htmlChallenge.answer, won:won }); 
+    }, 500);
 }
 
-router.post('/', function(req, res, next){
-    xssChallenge({prefix: "<html><body><div id='answer'><ul><li>", answer: req.body.answer, suffix: "</li></ul></div></body></html>"}, res, "xss");
-});
 
 
 
